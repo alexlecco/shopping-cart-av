b5605560b4fde738d53355bd36d8acbb84418553
alexlecco <alexlecco24@gmail.com>
2020-05-30 11:02:37 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ProductsContainer from './containers/ProductsContainer'
+import CartContainer from './containers/CartContainer'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h2>Shopping Cart Example</h2>
+      <hr/>
+      <ProductsContainer />
+      <hr/>
+      <CartContainer />
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,15 +1,36 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
+import { createLogger } from 'redux-logger'
+import thunk from 'redux-thunk'
+import { createStore, applyMiddleware, compose } from 'redux'
+import reducer from './reducers'
+import { getAllProducts } from './actions'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+
+const middleware = [ thunk ];
+if (process.env.NODE_ENV !== 'production') {
+  middleware.push(createLogger());
+}
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  reducer,
+  composeEnhancer(applyMiddleware(...middleware))
+)
+
+store.dispatch(getAllProducts())
+
+render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
-);
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
